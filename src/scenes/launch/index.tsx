8e333ba322b69991cfcb5f86144a009e9df0beda
Shashi Sekhar <shashi.sekhar@optimizeitsystems.com>
2022-12-29 10:26:22 +0530

    added splashscreen.tsx

--- src/scenes/launch/index.tsx
+++ src/scenes/launch/index.tsx
@@ -1,16 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import * as Updates from "expo-updates";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Animated,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
 
 const styles = StyleSheet.create({
   container: {
-
+    // backgroundColor: "plum",
   },
 });
 
 
+
 export default function LaunchScreen() {
+  const onReloadPress = useCallback(() => {
+    if (Platform.OS === "web") {
+      location.reload();
+    } else {
+      Updates.reloadAsync();
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx </Text>
+      <Text
+        style={{
+          color: "black",
+          fontSize: 30,
+          marginBottom: 15,
+          fontWeight: "bold",
+        }}
+      >
+        Pretty Cool!!!
+      </Text>
+      <Button title="Run Again" onPress={onReloadPress} />
     </View>
   );
 }
