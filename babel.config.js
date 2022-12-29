b06d9b34a156708b237573ed9db2ab7f7986e2d0
Shashi Sekhar <shashi.sekhar@optimizeitsystems.com>
2022-12-29 22:04:04 +0530

    added theme

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,16 @@ module.exports = function (api) {
         },
       ],
     ],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin", 
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@app': './src',
+          },
+        },
+      ],
+    ],
   };
 };
